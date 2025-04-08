8ea99bf4eb7f945140ece3df0eb341bdc1f207f7
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2025-04-08 13:20:33 +0200

    Fix test

--- test/e2e/turbopack-turbo-config-compatibility/index.test.ts
+++ test/e2e/turbopack-turbo-config-compatibility/index.test.ts
@@ -1,26 +1,24 @@
+/* eslint-disable jest/no-standalone-expect */
 import { nextTestSetup } from 'e2e-utils'
+;(process.env.IS_TURBOPACK_TEST ? describe : describe.skip)(
+  'turbopack-turbo-config-compatibility',
+  () => {
+    describe('including both turbopack and deprecated experimental turbo config', () => {
+      const { next } = nextTestSetup({
+        files: __dirname,
+      })
 
-describe('turbopack-turbo-config-compatibility', () => {
-  describe('including both turbopack and deprecated experimental turbo config', () => {
-    const { next, isTurbopack } = nextTestSetup({
-      files: __dirname,
+      it('prefers turbopack config over deprecated experimental turbo config', async () => {
+        const $ = await next.render$('/')
+        expect($('#result').text()).toEqual('Hello turbopack')
+      })
     })
 
-    if (!isTurbopack) {
-      return
-    }
-
-    it('prefers turbopack config over deprecated experimental turbo config', async () => {
-      const $ = await next.render$('/')
-      expect($('#result').text()).toEqual('Hello turbopack')
-    })
-  })
-
-  describe('only including deprecated experimental turbo config', () => {
-    const { next, isTurbopack } = nextTestSetup({
-      files: __dirname,
-      overrideFiles: {
-        'next.config.js': `module.exports = {
+    describe('only including deprecated experimental turbo config', () => {
+      const { next } = nextTestSetup({
+        files: __dirname,
+        overrideFiles: {
+          'next.config.js': `module.exports = {
           experimental: {
             turbo: {
               resolveAlias: {
@@ -29,16 +27,13 @@ describe('turbopack-turbo-config-compatibility', () => {
             },
           },
         }`,
-      },
-    })
+        },
+      })
 
-    if (!isTurbopack) {
-      return
-    }
-
-    it('still uses the deprecated experimental turbo config', async () => {
-      const $ = await next.render$('/')
-      expect($('#result').text()).toEqual('Hello turbo')
+      it('still uses the deprecated experimental turbo config', async () => {
+        const $ = await next.render$('/')
+        expect($('#result').text()).toEqual('Hello turbo')
+      })
     })
-  })
-})
+  }
+)
